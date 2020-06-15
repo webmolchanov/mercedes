a5d8981c61172134debfd0f8945ae9dfa908fa41
Oleg Molchanov <63306317+webmolchanov@users.noreply.github.com>
2020-06-15 14:21:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// JS-ФУНКЦИЯ ОПРЕДЕЛЕНИЯ ПОДДЕРЖКИ WEBP
+
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
+});
+
+// ========================================================================
+
+// IBG
+function ibg(){
+
+  let ibg=document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+  if(ibg[i].querySelector('img')){
+  ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+  }
+  }
+  }
+  
+  ibg();
+// ========================================================================
+
+
+// hamburger menu
+
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+  document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+}
+
+$(document).ready(function(){
+  $('.menu-icon-wrapper').click(function(event){
+    $('.menu__list').slideToggle();
+    $('body').toggleClass('lock');
+  });
+  
+});
+
+// ========================================================================
+$(function(){
+  $('.slider').slick({
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true
+});
+
+$('.content-reviews').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+});
+
+$('.slider-buy').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+});
